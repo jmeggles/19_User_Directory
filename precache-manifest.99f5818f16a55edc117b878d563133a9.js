9d0127a0a9a1d544736a470526a4fc130582717b
Jody Eggleston <jmeggles@gmail.com>
2020-08-09 20:26:39 -0400

    Updates

---
+++ precache-manifest.99f5818f16a55edc117b878d563133a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38f0b6b1c091bac0e326ca301fe0ee8",
+    "url": "/19_User_Directory/index.html"
+  },
+  {
+    "revision": "2092022757e434274d49",
+    "url": "/19_User_Directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "607dd0d152c8dd8a8425",
+    "url": "/19_User_Directory/static/js/2.54514c41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19_User_Directory/static/js/2.54514c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2092022757e434274d49",
+    "url": "/19_User_Directory/static/js/main.13bf6835.chunk.js"
+  },
+  {
+    "revision": "eb168631c7b2266c2461",
+    "url": "/19_User_Directory/static/js/runtime-main.be29e720.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/19_User_Directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
