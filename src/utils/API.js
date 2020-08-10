725583b7ed012fdd1d69ab1d6fba20e88f41c9e1
Jody Eggleston <jmeggles@gmail.com>
2020-08-09 21:47:49 -0400

    updated api info and part of the readme

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,13 +3,8 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
+  getUsers: function () {
+    // returns 100 random users with US information per the site: https://randomuser.me/documentation#howto
+    return axios.get("https://randomuser.me/api/?results=100&nat=us");
   }
 };
